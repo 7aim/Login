50513bc9774ab88af134c840080f5ef57da6897e
Aim <7asim6@gmail.com>
2025-03-09 16:12:47 +0400

    10 Mart

---
+++ login.py
@@ -0,0 +1,38 @@
+users = {}
+#logged_in = None
+
+while True:
+
+    print("\n1-Register\n2-Login\n3-Reset password\n4-Logout\n")
+    secim = input("1/2/3/4 : ")
+    
+    if secim == "1":
+        user = input("Username: ")
+        if user not in users:
+            users[user] = input("Şifre: ")
+            print("\nRegistration complete")
+        else:
+            print("\nUser already exists")
+    
+    if secim == "2":
+        user = input("UsernAme: ")
+        if users[user] == input("Şifre: "):
+            #logged_in = user
+            print(f"\nWelcome {user}!")
+        else:
+            print("\nIncorrect input")
+    
+    if secim == "3":
+        user = input("Username: ")
+        if user in users:
+            users[user] = input("NEw password: ")
+            print("\nPassword changed!")
+        else:
+            print("\nNo user")
+    
+    if secim == "4":
+        print("Exited")
+        #logged_in = None
+
+
+        
